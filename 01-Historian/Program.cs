bd9024bfe4295cd3983da1e88a5ca09079498096
Daniel Björk <da.bjork@cgi.com>
2024-12-02 23:08:01 +0100

    Add project files.

---
+++ 01-Historian/Program.cs
@@ -0,0 +1,16 @@
+﻿
+var lines = File.ReadAllLines("input.txt");
+var left = lines.Select(x => int.Parse(x[..5])).Order().ToArray();
+var right = lines.Select(x => int.Parse(x[^5..])).Order().ToArray();
+
+int sum = 0;
+for (int i = 0; i < left.Length; i++)
+    sum += Math.Abs(left[i] - right[i]);
+
+Console.WriteLine(sum);
+
+int score = 0;
+foreach (var n in left)
+    score += right.Count(x => x == n) * n;
+
+Console.WriteLine(score);
