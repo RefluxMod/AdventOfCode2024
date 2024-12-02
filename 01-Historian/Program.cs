d7177eaafcbb4cba9d9f717464bf2476a17ed7e4
Daniel Björk <da.bjork@cgi.com>
2024-12-03 00:02:21 +0100

    Day 1

--- 01-Historian/Program.cs
+++ 01-Historian/Program.cs
@@ -1,16 +1,5 @@
-﻿
-var lines = File.ReadAllLines("input.txt");
-var left = lines.Select(x => int.Parse(x[..5])).Order().ToArray();
-var right = lines.Select(x => int.Parse(x[^5..])).Order().ToArray();
-
-int sum = 0;
-for (int i = 0; i < left.Length; i++)
-    sum += Math.Abs(left[i] - right[i]);
-
-Console.WriteLine(sum);
-
-int score = 0;
-foreach (var n in left)
-    score += right.Count(x => x == n) * n;
-
-Console.WriteLine(score);
+﻿var lines = File.ReadAllLines("input.txt");
+var left = lines.Select(x => int.Parse(x[..5])).Order();
+var right = lines.Select(x => int.Parse(x[^5..])).Order();
+Console.WriteLine(left.Zip(right, (a, b) => Math.Abs(a - b)).Sum());
+Console.WriteLine(left.Sum(x => right.Count(r => r == x) * x));
