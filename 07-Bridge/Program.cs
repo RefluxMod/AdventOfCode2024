4a89ed2063933ce7fc12c756fe59739dfce8ce0b
dabjork <da.bjork@outlook.com>
2024-12-26 17:49:40 +0100

    Day 7 and 8.

---
+++ 07-Bridge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using Op = System.Func<long, long, long>;
+
+var rows = from l in File.ReadAllLines("input.txt")
+           let m = Regex.Matches(l, @"\d+").Select(x => long.Parse(x.Value))
+           select (Target: m.First(), Nums: m.Skip(1).ToArray());
+
+Op[] ops = { (x, y) => x + y, (x, y) => x * y, (x, y) => long.Parse($"{x}{y}") };
+
+Console.WriteLine(Sum(ops[..2]));
+Console.WriteLine(Sum(ops));
+
+long Sum(Op[] ops) =>
+    rows.Sum(x => Results(ops, x.Nums).Contains(x.Target) ? x.Target : 0);
+
+IEnumerable<long> Results(Op[] ops, long[] nums, long acc = 0) =>
+    nums is [] ? [acc] : ops.SelectMany(f => Results(ops, nums[1..], f(acc, nums[0])));
---
+++ 08-Resonance/Program.cs
@@ -0,0 +1,37 @@
+﻿var grid = File.ReadAllLines("input.txt");
+
+var height = grid.Length;
+var width = grid[0].Length;
+
+var antennas =
+    from y in Enumerable.Range(0, height)
+    from x in Enumerable.Range(0, width)
+    where grid[y][x] != '.'
+    select (X: x, Y: y, V: grid[y][x]);
+
+Console.WriteLine(GetPositions(false).Count);
+Console.WriteLine(GetPositions(true).Count);
+
+
+HashSet<(int, int)> GetPositions(bool harmonics) =>
+    (from a1 in antennas
+     from a2 in antennas
+     where a1 != a2 && a1.V == a2.V
+     from n in GetAntinodes(a1.X, a1.Y, a2.X, a2.Y, harmonics)
+     select n).ToHashSet();
+
+IEnumerable<(int X, int Y)> GetAntinodes(int x1, int y1, int x2, int y2, bool harmonics)
+{
+    int dirX = x2 - x1;
+    int dirY = y2 - y1;
+    int nX = harmonics ? x2 : x2 + dirX;
+    int nY = harmonics ? y2 : y2 + dirY;
+
+    while (nX >= 0 && nX < width && nY >= 0 && nY < height)
+    {
+        yield return (nX, nY);
+        if (!harmonics) break;
+        nX += dirX;
+        nY += dirY;
+    }
+}
